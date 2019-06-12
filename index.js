c6a089ac61326a04a03d25de785337629de55635
Valentin Charoux <valentin20charoux@gmail.com>
2019-06-12 15:39:57 +0400

    Change send method with Return Key

--- index.js
+++ index.js
@@ -8,6 +8,7 @@ var server_port = process.env.OPENSHIFT_NODEJS_PORT || 8080;
 var server_ip_addr = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1';
 
 var server = app.listen(server_port, () => {
+	console.log("Listening on address : " + server_ip_addr);
 	console.log("Listening on port : " + server_port);
 });
 
@@ -25,6 +26,14 @@ io.on('connection', (socket) => {
 	//	When the server recieves a message from a socket
 	socket.on('chat', (data) => {
 		io.sockets.emit('chat', data);
+
+		let today = Date.now();
+
+		let today_str = today.getDate() + "/" + (today.getMonth() + 1) + "/" + today.getFullYear() + " at " + today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
+
+		console.log(today_str);
+		console.log(data.username + "(" + socket.id + ") : " + data.message);
+		console.log('\n');
 	});
 
 	//Typing event
--- public/main.js
+++ public/main.js
@@ -11,13 +11,21 @@ var btnSend = document.getElementById('send');
 var boxOutput = document.getElementById('output');
 var boxFeedBack = document.getElementById('feedback');
 
-//Emit Events
-btnSend.addEventListener('click', () => {
+function sendMessage() {
 	socket.emit('chat', {
 		message: editxtMessage.value,
 		handle: editxtUsername.value
 	});
-});
+}
+
+//Emit Events
+btnSend.addEventListener('click', sendMessage);
+
+editxtMessage.onkeypress = (e) => {
+	if(e.code == "Enter") {
+		sendMessage();
+	}
+};
 
 editxtMessage.addEventListener('keypress', () => {
 	socket.emit('typing', editxtUsername.value);
